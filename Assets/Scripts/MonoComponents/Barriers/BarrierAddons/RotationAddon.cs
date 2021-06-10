3dcfd46167f0f7d96a6e77a266dcd194142a1e74
danil777lar <danil777lar@gmail.com>
2021-06-10 16:56:23 +0300

    rotation addon

---
+++ Assets/Scripts/MonoComponents/Barriers/BarrierAddons/RotationAddon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationAddon : MonoBehaviour
+{
+    enum RotDirection { left, right }
+
+    [SerializeField] RotDirection direction;
+    [SerializeField] float speed;
+    [SerializeField] float radius = 6f;
+
+    [Space, SerializeField] Transform cylinder;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (cylinder)
+        {
+            cylinder.transform.localScale = new Vector3(radius, cylinder.transform.localScale.y, radius);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float rotation = speed * (direction == RotDirection.left ? 1 : -1);
+        cylinder.localRotation *= Quaternion.Euler(Vector3.up * rotation);
+    }
+}
