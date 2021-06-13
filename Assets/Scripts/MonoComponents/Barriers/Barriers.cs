6f223a9d7658b1c685e118f4747cd97cc73da149
glebkhaletsky <order.khaletsky.gleb@gmail.com>
2021-06-13 23:38:45 +0300

    add-barriers

---
+++ Assets/Scripts/MonoComponents/Barriers/Barriers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TeamAlpha.Source
+{
+    public enum TypeBarrier
+    {
+        Destroyed,
+        Undestroyded
+    }
+
+    public class Barriers : MonoBehaviour
+    {
+        [SerializeField] private int damageValue;
+        [SerializeField] private TypeBarrier currentTypeBarriers;
+
+
+        protected virtual void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == DataGameMain.LayerPlayer)
+            {
+                PlayerController.Current.SendDamage(damageValue);
+            }
+        }
+    }
+
+    public interface IButton
+    {        
+        void PushButton(float speed);
+    }
+}
---
+++ Assets/Scripts/MonoComponents/Barriers/ButtonForBarriers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace TeamAlpha.Source
+{    
+    public class ButtonForBarriers : MonoBehaviour, IButton
+    {
+        [SerializeField] private float responseSpeed;
+        [SerializeField] private Barriers barrierPrefab;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == DataGameMain.LayerPlayer)
+            {
+                transform.DOLocalMove(new Vector3(0, 0.1f, 0), 0.5f, false).OnComplete(()=> PushButton(responseSpeed));
+            }
+        }
+        public void PushButton(float speed)
+        {
+            barrierPrefab.GetComponent<IButton>().PushButton(speed);
+        }
+    }
+}
