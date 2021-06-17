f8ea2f87ae94e1838bd7cedb0b117a05d8199eb0
glebkhaletsky <order.khaletsky.gleb@gmail.com>
2021-06-17 22:36:39 +0300

    add-coin

--- Assets/Scripts/MonoComponents/LevelObjects/Coin.cs
+++ Assets/Scripts/MonoComponents/LevelObjects/Coin.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 using Sirenix.OdinInspector;
+
 
 namespace TeamAlpha.Source
 {
-    public class Part : MonoBehaviour
+    public class Coin : MonoBehaviour
     {
-        public GameObject Effect;
-
         [SerializeField] bool randomRotate;
-        [ShowIf("@randomRotate==false")]
+        [ShowIf ("@randomRotate==false")]
         [SerializeField, Range(20, 80)] private float rotationSpeed;
+        private PanelCoin panel;        
+
+        private void Start()
+        {
+            panel = FindObjectOfType<PanelCoin>();
+        }
         private void Update()
         {
             if (randomRotate)
@@ -23,17 +27,17 @@ namespace TeamAlpha.Source
             else
             {
                 transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
-            }
+            }            
         }
-
         private void OnTriggerEnter(Collider other)
         {
-            if (other.gameObject.layer == DataGameMain.LayerPlayer) 
+            if(other.gameObject.layer == DataGameMain.LayerPlayer)
             {
-                PlayerController.Current.SendPart();
-                ParticleSystem destroyParts = Instantiate(Effect, other.transform).GetComponent<ParticleSystem>();
+                panel.UpdateCoinText(1);
                 Destroy(gameObject);
             }
+            
         }
+
     }
 }
---
+++ Assets/Scripts/UI/PanelCoin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+namespace TeamAlpha.Source
+{
+    public class PanelCoin : MonoBehaviour
+    {
+        [SerializeField] private Text coinText;
+        private static int allCoin;
+        
+       
+
+        private void Start()
+        {
+            coinText.text = allCoin.ToString();
+        }
+
+        public void UpdateCoinText(int value)
+        {
+            allCoin += value;
+            Debug.Log(allCoin);
+            coinText.text = allCoin.ToString();
+        }
+    }
+}
