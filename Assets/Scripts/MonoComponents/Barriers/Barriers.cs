940ef0826b371af5dd785d5494918039fc4d276a
glebkhaletsky <order.khaletsky.gleb@gmail.com>
2021-06-21 13:22:53 +0300

    add-enemy-robot-and-fix-barriers-settings

--- Assets/Scripts/MonoComponents/Barriers/Barriers.cs
+++ Assets/Scripts/MonoComponents/Barriers/Barriers.cs
@@ -10,22 +10,38 @@ namespace TeamAlpha.Source
     {
         [Header("Damage")]        
         [SerializeField] protected int damageValue;
-        [SerializeField] private bool nonTrip;
         [Header("Object Behavior")]
         [SerializeField] private bool breakingObject;
         [ShowIf("breakingObject")]
+        [SerializeField] private bool breakingByBullet;
+        [ShowIf("breakingByBullet")]
+        [SerializeField] protected int health;
+        [ShowIf("breakingObject")]
         [SerializeField] private List<GameObject> partsBarrier = new List<GameObject>();
-         
+
         protected virtual void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.layer == DataGameMain.LayerPlayer)
             {
                 PlayerController.Current.SendDamage(damageValue);
-                if(breakingObject)
+                if (breakingObject)
                 {
                     Collider collider = GetComponentInChildren<Collider>();
                     collider.enabled = false;
                     Broken();
+                }
+            }
+            if (other.gameObject.GetComponent<Bullet>())
+            {
+                if (breakingByBullet)
+                {
+                    health -= 1;
+                    if (health <= 0)
+                    {
+                        Collider collider = GetComponentInChildren<Collider>();
+                        collider.enabled = false;
+                        Broken();
+                    }                    
                 }
             }
         }
@@ -38,7 +54,7 @@ namespace TeamAlpha.Source
                 Vector3 forceDirection = (partsBarrier[i].transform.position - transform.position).normalized;
                 Rigidbody rb = partsBarrier[i].GetComponent<Rigidbody>();
                 rb.AddForce(3 * forceDirection, ForceMode.Impulse);
-                rb.AddTorque(randomVector * 3f, ForceMode.Impulse);
+                rb.AddTorque(randomVector * 3f, ForceMode.Impulse);                
                 if(partsBarrier[i]!= null)
                 {
                     Destroy(partsBarrier[i], 3f);
---
+++ Assets/Scripts/MonoComponents/Barriers/EnemyBarrier.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dreamteck.Splines;
+using Animancer;
+
+
+namespace TeamAlpha.Source
+{
+
+    public class EnemyBarrier : Barriers
+    {
+        
+        [Header("Animation")]
+        [SerializeField] private NamedAnimancerComponent _animacer;
+        [SerializeField] private AnimationClip _animRun;
+        [SerializeField] private AnimationClip _animIdle;
+        [Header("Move")]
+        [SerializeField] private SplineFollower splineFollower;
+        private void Start()
+        {
+            LayerDefault.Default.OnPlayStart += () =>
+            {
+                splineFollower.followSpeed = 8f;
+                _animacer.Play(_animRun, 0.15f);
+            };
+            _animacer.Play(_animIdle, 0.15f);
+            splineFollower.followSpeed = 0f;
+        }
+        protected override void OnTriggerEnter(Collider other)
+        {            
+            base.OnTriggerEnter(other);
+            if (base.health<=0)
+            {
+                splineFollower.followSpeed = 0f;
+                _animacer.Stop();
+            }                       
+        }
+
+    }
+}
