f38c974893649e2e2f94c4c0cb4c41d51f7ec672
glebkhaletsky <order.khaletsky.gleb@gmail.com>
2021-06-22 23:48:34 +0300

    fix-finish

--- Assets/Scripts/MonoComponents/LevelObjects/Finish.cs
+++ Assets/Scripts/MonoComponents/LevelObjects/Finish.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace TeamAlpha.Source
 {
@@ -8,23 +9,23 @@ namespace TeamAlpha.Source
     {
         private Transform _player { get => PlayerController.Current.transform; }
         [SerializeField] float distanceToActive;
-        [SerializeField] List<Lantern> lanterns = new List<Lantern>();
+        [SerializeField] List<GameObject> lights = new List<GameObject>();
 
         private void Update()
         {
             float distance = Vector3.Distance(transform.position, _player.position);
             if(distance< distanceToActive)
             {
-                for (int i = 0; i < lanterns.Count; i++)
+                for (int i = 0; i < lights.Count; i++)
                 {
-                    lanterns[i].gameObject.SetActive(true);
+                    lights[i].transform.DOScale(new Vector3(1f, 1f, 1.75f), 2f);
                 }
             }
             else
             {
-                for (int i = 0; i < lanterns.Count; i++)
+                for (int i = 0; i < lights.Count; i++)
                 {
-                    lanterns[i].gameObject.SetActive(false);
+                    lights[i].transform.DOScale(new Vector3(0.0005f, 0.0005f, 0.0005f), 0f);
                 }
             }
         }
