80eafdca3094343febd0b5e4b8a20b9eec781e98
glebkhaletsky <order.khaletsky.gleb@gmail.com>
2021-06-26 21:00:13 +0300

    fix-broken-detail

---
+++ Assets/Scripts/MonoComponents/PreAttachMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dreamteck.Splines;
+
+namespace TeamAlpha.Source
+{
+    public class PreAttachMove : MonoBehaviour
+    {
+        [SerializeField] private SplineFollower splineFollower;
+        private void Start()
+        {
+            splineFollower.followSpeed = FindObjectOfType<PlayerController>().MovingObject.Speed;
+        }
+    }
+}
