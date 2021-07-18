b332561e2b841edc50d66d72612ac0d7d5d66140
glebkhaletsky <order.khaletsky.gleb@gmail.com>
2021-07-19 00:17:22 +0300

    neighbour-barrel

---
+++ Assets/Scripts/MonoComponents/Barriers/Barrel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TeamAlpha.Source
+{
+    public class Barrel : Barriers
+    {
+        [SerializeField] private float minDistance =5;
+        [SerializeField] private List<Barrel> neighbourBarrel = new List<Barrel>();
+
+        private void Start()
+        {
+            Barrel[] allBarrels = FindObjectsOfType<Barrel>();
+            for (int i = 0; i < allBarrels.Length; i++)
+            {
+                float distance = Vector3.Distance(allBarrels[i].transform.position, transform.position);
+                if(distance<= minDistance)
+                {
+                    neighbourBarrel.Add(allBarrels[i]);
+                }
+            }
+        }
+
+        protected override void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Bullet>())
+            {
+                for (int i = 0; i < neighbourBarrel.Count; i++)
+                {
+                    neighbourBarrel[i].Broken(5);
+                }
+            }
+            base.OnTriggerEnter(other);
+        }
+    }
+}
